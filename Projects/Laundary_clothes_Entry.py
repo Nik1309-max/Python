43e0e3e841387e6ef5cf9da2716f6172eb336b16
Nikhil Kumar <alkaofshalimpur@gmail.com>
2026-02-12 22:01:10 +0530

    Add files via upload

---
+++ Projects/Laundary_clothes_Entry.py
@@ -0,0 +1,13 @@
+# while True:
+#     print("Select any of these:\n1.) Add clothes\n2.) Delete data 3.) Exit")
+#     inp = input("Select 1,2 or 3: ").strip()
+#     if inp=="1":
+#         pass
+#     elif inp=="2":
+#         pass
+#     elif inp=="3":
+#         print("Exitted from the program successfully.")
+#         break
+#     else:
+#         print("****************** Error 404 ******************")
+
